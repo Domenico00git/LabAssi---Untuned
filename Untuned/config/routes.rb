ce9a568eeb646442cbf592cc8e78cb1107606920
marioiacobelli <iacobelli.1841427@studenti.uniroma1.it>
2023-07-21 19:05:11 +0200

    modifica views devise

--- Untuned/config/routes.rb
+++ Untuned/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  as:user do 
+    get "signin" => 'devise/sessions#new'
+    delete "signout" => 'devise/sessions#destroy'
+    get "signup" => 'devise/registrations#new'
+  end
   root 'pages#home'
-  get 'signup' => 'pages#signup'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'contact' => 'pages#contact'
 end
