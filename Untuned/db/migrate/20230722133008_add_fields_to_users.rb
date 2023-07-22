582b17324c02bc0e07af9bb866639cb6fcc773c5
marioiacobelli <iacobelli.1841427@studenti.uniroma1.it>
2023-07-22 16:42:33 +0200

    aggiornamento campi dati User e modifica di alcune view

---
+++ Untuned/db/migrate/20230722133008_add_fields_to_users.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    add_column :users, :name, :string
+    add_column :users, :lastname, :string
+    add_column :users, :country, :string
+    add_column :users, :birthdate, :date
+  end
+end
