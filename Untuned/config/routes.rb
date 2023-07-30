d7940948c55b3cb03d609f6aae6ea743a4aa310c
mario <iacobelli.1841427@studenti.uniroma1.it>
2023-07-30 18:04:29 +0200

    implementazione filtri per i post per visualizzarli in ordine di caricamento crescente/decrescente e per visualizzare i post di tutti o dei soli folllowing

--- Untuned/config/routes.rb
+++ Untuned/config/routes.rb
@@ -9,36 +9,39 @@ Rails.application.routes.draw do
 
   resources :users, only: :show, param: :username do
     member do
-      get "follow", to: "follows#create"
-      get "unfollow", to: "follows#destroy"
+      get 'follow', to: 'follows#create'
+      get 'unfollow', to: 'follows#destroy'
     end
   end
 
   resources :posts do
     member do
-      get "like", to: "votes#createlike"
-      get "unlike", to: "votes#destroylike"
+      get 'like', to: 'votes#createlike'
+      get 'unlike', to: 'votes#destroylike'
 
-      get "dislike", to: "votes#createdislike"
-      get "undislike", to: "votes#destroydislike"
+      get 'dislike', to: 'votes#createdislike'
+      get 'undislike', to: 'votes#destroydislike'
     end
   end
 
 
   devise_for :users
   as:user do 
-    get "signin", to: 'devise/sessions#new'
-    get "signout", to: 'devise/sessions#destroy'
-    get "signup", to: 'devise/registrations#new'
-    get "edit", to: 'devise/registration#edit'
+    get 'signin', to: 'devise/sessions#new'
+    get 'signout', to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+    get 'edit', to: 'devise/registration#edit'
   end
 
   root 'pages#home'
   get 'contact', to: 'pages#contact'
   get "posts", to: 'posts#index'
-  get "admin", to: 'root#admin'
+  get 'oldest_posts', to: 'posts#oldest'
+  get 'newest_following_posts', to: 'posts#newest_following'
+  get 'oldest_following_posts', to: 'posts#oldest_following'
+  get 'admin', to: 'root#admin'
 
-  get "followers", to: 'users#followers'
-  get "following", to: 'users#following'
+  get 'followers', to: 'users#followers'
+  get 'following', to: 'users#following'
 
 end
