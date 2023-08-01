71df88e2d5b953f6fb379d31ad487a8c8605db6a
mario <iacobelli.1841427@studenti.uniroma1.it>
2023-08-01 14:26:18 +0200

    versione aggiornata e funzionante

--- Untuned/db/schema.rb
+++ Untuned/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_31_104107) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_29_155459) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -94,6 +94,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_104107) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
@@ -101,6 +104,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_104107) do
     t.string "lastname"
     t.date "birthdate"
     t.boolean "admin", default: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
@@ -124,7 +128,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_31_104107) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "comments", "posts"
-  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
