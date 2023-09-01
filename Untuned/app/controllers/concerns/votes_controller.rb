5d44b223704bd819def5ce572f02fabbddd1ee46
mario <iacobelli.1841427@studenti.uniroma1.it>
2023-09-01 16:27:45 +0200

    modifica della redirect dopo like o dislike a un post

--- Untuned/app/controllers/concerns/votes_controller.rb
+++ Untuned/app/controllers/concerns/votes_controller.rb
@@ -4,25 +4,25 @@ class VotesController < ApplicationController
     def createlike
         post = Post.find(params[:id])
         post.liked_by current_user
-        redirect_to posts_path, notice: "You successfully liked a post"
+        redirect_back_or_to root_path, notice: "You successfully liked a post"
     end
 
     def destroylike
         post = Post.find(params[:id])
         post.unliked_by current_user
-        redirect_to posts_path, notice: "You successfully unliked a post"
+        redirect_back_or_to root_path, notice: "You successfully unliked a post"
     end
 
     def createdislike
         post = Post.find(params[:id])
         post.disliked_by current_user
-        redirect_to posts_path, notice: "You successfully disliked a post"
+        redirect_back_or_to root_path, notice: "You successfully disliked a post"
     end
 
     def destroydislike
         post = Post.find(params[:id])
         post.undisliked_by current_user
-        redirect_to posts_path, notice: "You successfully undisliked a post"
+        redirect_back_or_to root_path, notice: "You successfully undisliked a post"
     end
   end
   
